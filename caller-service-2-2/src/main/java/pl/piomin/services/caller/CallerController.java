2973410c6892ceb2737cb04e91f3b59e9f3c6fcf
Julien COGNET <julien.cognet@cgi.com>
2021-03-30 08:59:24 +0200

    Tried several lib levels

---
+++ caller-service-2-2/src/main/java/pl/piomin/services/caller/CallerController.java
@@ -0,0 +1,41 @@
+package pl.piomin.services.caller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/caller22")
+public class CallerController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CallerController.class);
+
+	@Autowired
+	Environment environment;
+	@Autowired
+	RestTemplate template;
+
+	@GetMapping
+	public String call() {
+		String url = "http://callme-service/callme";
+		String callmeResponse = template.getForObject(url, String.class);
+		LOGGER.info("Response: {}", callmeResponse);
+		return "I'm Caller 2.2 running on port " + environment.getProperty("local.server.port")
+				+ " calling-> " + callmeResponse;
+	}
+
+	@GetMapping("/slow")
+	public String slow() {
+		String url = "http://callme-service/callme/slow";
+		String callmeResponse = template.getForObject(url, String.class);
+		LOGGER.info("Response: {}", callmeResponse);
+		return "I'm Caller 2.2 running on port " + environment.getProperty("local.server.port")
+				+ " calling-> " + callmeResponse;
+	}
+}
--- generic-java-docker/entrypoint.sh
+++ generic-java-docker/entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 echo "The application will start in 10s..." && sleep 10
+
+echo ">> Starting consul agent"
+consul agent -retry-join consul-server-bootstrap -client 0.0.0.0 -data-dir=/consul/data -config-dir=/consul/config &
+
+sleep 5
+
+echo ">> Starting Java application"
 for f in /app/*.jar
 do
     cp $f /tmp/app.jar
 done
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "/tmp/app.jar" "$@"
+exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "/tmp/app.jar" "$@" &
+
+tail -f /dev/null
 
